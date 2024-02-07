59d379dbca805f97ec204a82b6ebc5d8e290d243
Shahad <ssalehalanbari@stu.kau.edu.sa>
2024-02-07 13:29:20 +0300

    second commit

--- src/main.java
+++ src/main.java
@@ -16,6 +16,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello github world");
+          System.out.println("Hello github world 3333");
     }
     
 }
